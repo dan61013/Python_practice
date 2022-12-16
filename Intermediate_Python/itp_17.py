62c65d84a8a7d51fbc33c590e4bfc73acca98394
Dan Cai <zxc61013@gmail.com>
2022-12-16 18:13:22 +0800

    deque, namedtuple

--- Intermediate_Python/itp_17.py
+++ Intermediate_Python/itp_17.py
@@ -1,2 +1,67 @@
 # https://eastlakeside.gitbook.io/interpy-zh/collections
-# collections
+# collections, defauldict, counter
+
+from collections import defaultdict, Counter
+import collections, json
+
+def test_01():
+    
+    habits = (
+        ('Dan', 'Breaking'),
+        ('Dan', 'Anime'),
+        ('Mira', 'Anime'),
+        ('Mira', 'Make up'),
+        ('Ken', 'Reading')
+    )
+    
+    habits_dict = defaultdict(list)
+
+    for name, age in habits:
+        habits_dict[name].append(age)
+    print(habits_dict)
+
+def test_02():
+    some_dict = {}
+    some_dict['name']['habits'] = 'Breaking' # KeyError
+
+def test_02_solve():
+    """import collections 解決 test_02的KeyError"""
+    
+    tree = lambda: collections.defaultdict(tree)
+    some_dict = tree()
+    
+    some_dict['name']['habits'] = 'Breaking'
+    print(json.dumps(some_dict)) # {"name": {"habits": "Breaking"}
+
+def test_03():
+    
+    habits = (
+        ('Dan', 'Breaking'),
+        ('Dan', 'Anime'),
+        ('Mira', 'Anime'),
+        ('Mira', 'Make up'),
+        ('Ken', 'Reading')
+    )
+    
+    habits_counter = Counter(name for name, habit in habits)
+    print(habits_counter)
+
+def file_counter():
+    # 統計文件
+    with open('./Intermediate_Python/itp_01.py', 'rb') as f:
+        line_count = Counter(f)
+    print(line_count)
+
+def main():
+    test_01()
+    # test_02()
+    test_02_solve()
+    
+    # Counter
+    test_03()
+    
+    # file count
+    file_counter()
+    
+if __name__ == "__main__":
+    main()
---
+++ Intermediate_Python/itp_18.py
@@ -0,0 +1,63 @@
+# https://eastlakeside.gitbook.io/interpy-zh/collections
+# deque, namedtuple
+
+from collections import deque, namedtuple
+
+# deque
+def test_01():
+    """使用方式類似list操作"""
+    d = deque()
+    print(type(d), type(deque))
+    
+    d.append('1')
+    d.append(2)
+    d.append('33')
+    
+    print(len(d), d, d[0])
+    
+    d_02 = deque(range(5))
+    print(len(d_02))
+    print(d_02.popleft()) # delete from left [0]
+    print(d_02.pop()) # delete from right [-1]
+    print(d_02)
+
+def test_02():
+    
+    # 設定maxlen, 多出的參數，會從0開始被排除
+    d = deque(range(30), maxlen=15)
+    # deque([15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], maxlen=15)
+    print(d)
+    
+    # extendleft, extend，從左&右邊拓展
+    d_02 = deque([1, 2, 3, 4, 5])
+    d_02.extendleft([0, 1])
+    d_02.extend([10, 100, 101, 1001])
+    print(d_02)
+
+def test_03():
+    """namedtuple是把tuple變成一個針對簡單任務使用的容器，
+可以像字典一樣使用，是immutable"""
+    Animal = namedtuple('Animal', 'name age type')
+    cat_01 =  Animal('小黑', 1, 'cat')
+    print(cat_01.name, cat_01.type)
+
+def cal_01(a, b):
+    sum = a + b
+    multiply = a * b
+    calculate = namedtuple('calculate', 'sum multiply')
+    ans = calculate(sum, multiply)
+    return ans
+
+def main():
+    # deque
+    # test_01()
+    # test_02()
+    
+    # namedtuple
+    test_03()
+    
+    # namedtuple with function return
+    calculate = cal_01
+    
+if __name__ == "__main__":
+    main()
